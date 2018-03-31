75564c19f6c800ad96e87546381acd8a1f73c115
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-03-31 17:38:39 +0900

    Modify to use Emacs.app

--- init.el
+++ init.el
@@ -13,3 +13,20 @@
 ;; (define-key scheme-mode-map (kbd "C-M-u") 'backwark-up-lis)
 ;; (define-key scheme-mode-map (kbd "C-M-a") 'begining-of-defun)
 ;; (define-key scheme-mode-map (kbd "C-M-q") 'indent-pp-sexp)
+
+
+(add-to-list 'exec-path "/usr/local/bin")
+(setq scheme-program-name "racket")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
