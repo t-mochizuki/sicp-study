45f278fcbe92f9e3653091ed0844d0f598f84dea
Mochizuki Takamasa <t-mochizuki@users.noreply.github.com>
2018-04-05 18:33:05 +0900

    Add some useful plugins

--- init.el
+++ init.el
@@ -1,4 +1,61 @@
-(setq transient-mark-mode t)
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa"."http://melpa.org/packages/") t)
+(package-initialize)
+
+(ivy-mode t)
+(setq ivy-use-virtual-buffers t)
+(setq enable-recursive-minibuffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+;; (global-set-key (kbd "C-c C-r") 'ivy-resume)
+;; (global-set-key (kbd "<f6>") 'ivy-resume)
+;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
+;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+;; (global-set-key (kbd "C-c g") 'counsel-git)
+;; (global-set-key (kbd "C-c j") 'counsel-git-grep)
+;; (global-set-key (kbd "C-c k") 'counsel-ag)
+;; (global-set-key (kbd "C-c l") 'counsel-locate)
+;; (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+;; (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
+
+(require 'popwin)
+(setq display-buffer-function 'popwin:display-buffer)
+
+(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'company-mode-hook
+	  '(lambda ()
+	     (set-face-attribute 'company-tooltip nil
+				 :foreground "black" :background "lightgrey")
+	     (set-face-attribute 'company-tooltip-common nil
+				 :foreground "black" :background "lightgrey")
+	     (set-face-attribute 'company-tooltip-common-selection nil
+				 :foreground "white" :background "steelblue")
+	     (set-face-attribute 'company-tooltip-selection nil
+				 :foreground "black" :background "steelblue")
+	     (set-face-attribute 'company-preview-common nil
+				 :background nil :foreground "lightgrey" :underline t)
+	     (set-face-attribute 'company-scrollbar-fg nil
+				 :background "orange")
+	     (set-face-attribute 'company-scrollbar-bg nil
+				 :background "gray40")
+	     (define-key company-mode-map (kbd "C-i") 'company-complete)
+	     (define-key company-active-map (kbd "C-n") 'company-select-next)
+	     (define-key company-active-map (kbd "C-p") 'company-select-previous)
+	     (define-key company-search-map (kbd "C-n") 'company-select-next)
+	     (define-key company-search-map (kbd "C-p") 'company-select-previous)))
+
+(transient-mark-mode t)
 
 (define-key global-map (kbd "M-/") 'dabbrev-expand)
 (define-key global-map (kbd "C-x C-o") 'other-window)
@@ -8,10 +65,14 @@
 
 (add-to-list 'exec-path "/usr/local/bin")
 (setq scheme-program-name "mit-scheme")
+(setq inferior-lisp-program "sbcl")
 
+(require 'paredit)
 (add-hook 'scheme-mode-hook
 	  '(lambda ()
 	     (setq indent-tabs-mode nil)
+	     (enable-paredit-mode)
+	     (show-paren-mode t)
 	     (define-key scheme-mode-map (kbd "C-j") 'newline-and-indent)
 	     (define-key scheme-mode-map (kbd "C-M-k") 'kill-sexp)))
 
@@ -20,7 +81,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(company-idle-delay nil)
+ '(package-selected-packages
+   (quote
+    (markdown-mode ddskk smartrep smartparens parinfer paredit cider slime counsel swiper popwin magit company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
